57b9174486437be89ee159decf6f66f3bded500a
Evgeniya Zhukova <evgeniya.zhukova.toronto@gmail.com>
2022-01-25 12:07:09 -0500

    Update Java Strings Introduction.java

--- Java/Strings/Java Strings Introduction.java
+++ Java/Strings/Java Strings Introduction.java
@@ -1,24 +1,18 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Solution {
 
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        scanner.close();
-        if(N % 2 == 1 || (N >= 6 && N <= 20)){
-           System.out.println("Weird"); 
-        }
-        else{
-           System.out.println("Not Weird"); 
-        }   
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length() + B.length());  
+        int compare = A.compareTo(B);     
+        System.out.println(compare > 0 ? "Yes":"No");
+        A = A.substring(0, 1).toUpperCase() + A.substring(1);
+        B = B.substring(0, 1).toUpperCase() + B.substring(1);    
+        System.out.println(A.concat(" " + B));
     }
 }
